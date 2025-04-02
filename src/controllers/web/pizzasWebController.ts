707c160fa37ba4cf426e10fd3527d912993d3555
Junior Soares <jjwsoares@outlook.com>
2025-04-02 09:13:06 -0300

    Adiciona tipagem a alguns elementos

--- src/controllers/web/pizzasWebController.ts
+++ src/controllers/web/pizzasWebController.ts
@@ -1,12 +1,18 @@
+import { Pizza, PizzaSize } from "@prisma/client";
 import { Handler } from "express";
+
+interface PizzasApiResponse {
+    pizzas: Pizza[],
+    priceSize: PizzaSize[]
+}
 
 export class PizzasWebController {
     index: Handler = async (req, res, next) => {
         try {
             const response = await fetch("http://localhost:3000/api/pizzas")
-            const dados = await response.json()
-            const pizzas = dados.pizzas
-            const priceSize = dados.priceSize
+            const dados:PizzasApiResponse = await response.json()
+            const pizzas:Pizza[] = dados.pizzas
+            const priceSize:PizzaSize[] = dados.priceSize
 
             const user = {role: "Admin"}
 
